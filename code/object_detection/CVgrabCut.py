2754c8e1ae14c1e28c1b6baf868b0f14aeedf7ab
Aimee <aimee.feng24@gmail.com>
2019-12-02 10:16:41 -0500

    Foreground Detection
    Changed little values to find best set of criteria for images

--- code/object_detection/CVgrabCut.py
+++ code/object_detection/CVgrabCut.py
@@ -2,15 +2,15 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-image = cv.imread('010.jpg')
+image = cv.imread('004.jpg')
 
 mask = np.zeros(image.shape[:2], np.uint8)
 
 backgroundModel = np.zeros((1,65), np.float64)
 foregroundModel = np.zeros((1,65), np.float64)
 
-rectangle = (20,20, image.shape[0]-50,image.shape[1]-50)
-cv.grabCut(image, mask, rectangle, backgroundModel, foregroundModel, 3, cv.GC_INIT_WITH_RECT)
+rectangle = (20,20, image.shape[0]-5,image.shape[1]-5)
+cv.grabCut(image, mask, rectangle, backgroundModel, foregroundModel, 2, cv.GC_INIT_WITH_RECT)
 
 mask2 = np.where((mask==2)|(mask==0), 0, 1).astype('uint8')
 
--- code/object_detection/detectCVLib.py
+++ code/object_detection/detectCVLib.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
 
-im = cv2.imread('010.jpg')
+im = cv2.imread('002.jpg')
 
 bbox, label, conf = cv.detect_common_objects(im)
 
